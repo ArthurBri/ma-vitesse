187876ab348fcca9808c70bb1210ad8c49a8b02e
Arthur Brière <arthur.briere@outlook.fr>
2019-12-22 16:48:01 +0100

    server : added compression for files

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+const serveStatic = require("serve-static");
+var compression = require('compression');
 
-let app = express();
+
+var app = express()
+
+// compress all responses
+app.use(compression());
+
+const path = require('path');
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
