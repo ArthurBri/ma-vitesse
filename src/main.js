2ca85e6b87a9d0b11b7d067b931444fc37a2f5ba
Arthur Brière <arthur.briere@outlook.fr>
2020-03-29 21:50:15 +0200

    > Add vue i18n support

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import {store} from './store/store'
 import "@/assets/scss";
 import './registerServiceWorker'
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
 
 new Vue({
     store,
     render: h => h(App),
+    i18n,
+
     beforeCreate() {
         this.$store.commit('initializeStore');
     }
